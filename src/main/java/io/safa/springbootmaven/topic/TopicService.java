090d4ca69b7853613c3015d3910dac707591ad1f
serpil safa <serpilsafa@gmail.com>
2020-10-22 20:42:06 +0100

    http methos continues

--- src/main/java/io/safa/springbootmaven/topic/TopicService.java
+++ src/main/java/io/safa/springbootmaven/topic/TopicService.java
@@ -1,5 +1,6 @@
 package io.safa.springbootmaven.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,11 +9,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 	
-	private List<Topic> topics = Arrays.asList(
+	private List<Topic> topics = new ArrayList<>( Arrays.asList(
 			new Topic("1","Kotlin", "Android programming"),
 			new Topic("2","Swift", "IOS programming"),
 			new Topic("3","Java","Android programmming")
-			); 
+			)); 
 	
 	public List<Topic> getAllTopics(){
 		return topics;
@@ -20,6 +21,25 @@ public class TopicService {
 	
 	public Topic getTopic(String id) {
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopic(String id, Topic topic) {
+		for (int i = 0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			if(t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t-> t.getId().equals(id));
+		
 	}
 
 }
